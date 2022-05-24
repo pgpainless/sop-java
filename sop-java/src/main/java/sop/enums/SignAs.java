be2fd9de4947e3671d7f8f4a499532f2c80c291b
Paul Schaub <vanitasvitae@fsfe.org>
2022-05-24 21:07:50 +0200

    Add javadoc to SignAs

--- sop-java/src/main/java/sop/enums/SignAs.java
+++ sop-java/src/main/java/sop/enums/SignAs.java
@@ -5,6 +5,13 @@
 package sop.enums;
 
 public enum SignAs {
+    /**
+     * Signature is made over the binary message.
+     */
     Binary,
+
+    /**
+     * Signature is made over the message in text mode.
+     */
     Text
 }
