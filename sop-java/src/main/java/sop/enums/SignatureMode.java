360f2fba024ac3177f070006e8c811086c45e313
Paul Schaub <vanitasvitae@fsfe.org>
2023-04-17 13:56:43 +0200

    Document SignatureMode

--- sop-java/src/main/java/sop/enums/SignatureMode.java
+++ sop-java/src/main/java/sop/enums/SignatureMode.java
@@ -4,7 +4,22 @@
 
 package sop.enums;
 
+/**
+ * Enum referencing relevant signature types.
+ *
+ * @see <a href="https://www.rfc-editor.org/rfc/rfc4880#section-5.2.1">
+ *     RFC4880 §5.2.1 - Signature Types</a>
+ */
 public enum SignatureMode {
+    /**
+     * Signature of a binary document (<pre>0x00</pre>).
+     */
     binary,
+
+    /**
+     * Signature of a canonical text document (<pre>0x01</pre>).
+     */
     text
+
+    // Other Signature Types are irrelevant.
 }
