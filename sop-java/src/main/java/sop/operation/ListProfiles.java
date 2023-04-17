8a9f53553145d510e1478fe299715dd1f6663929
Paul Schaub <vanitasvitae@fsfe.org>
2023-04-17 14:04:06 +0200

    Add documentation to ListProfiles command

--- sop-java/src/main/java/sop/operation/ListProfiles.java
+++ sop-java/src/main/java/sop/operation/ListProfiles.java
@@ -8,8 +8,18 @@ import sop.Profile;
 
 import java.util.List;
 
+/**
+ * Subcommand to list supported profiles of other subcommands.
+ */
 public interface ListProfiles {
 
+    /**
+     * Provide the name of the subcommand for which profiles shall be listed.
+     * The returned list of profiles MUST NOT contain more than 4 entries.
+     *
+     * @param command command name (e.g. <pre>generate-key</pre>)
+     * @return list of profiles.
+     */
     List<Profile> subcommand(String command);
 
 }
