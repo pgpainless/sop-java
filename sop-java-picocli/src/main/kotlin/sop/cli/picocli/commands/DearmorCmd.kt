688b8043a26cce2a80807db136ababd656cb978a
Paul Schaub <vanitasvitae@fsfe.org>
2023-11-04 17:45:56 +0100

    Kotlin conversion: DearmorCmd

---
+++ sop-java-picocli/src/main/kotlin/sop/cli/picocli/commands/DearmorCmd.kt
@@ -0,0 +1,37 @@
+package sop.cli.picocli.commands
+
+import java.io.IOException
+import picocli.CommandLine.Command
+import sop.cli.picocli.SopCLI
+import sop.exception.SOPGPException
+import sop.exception.SOPGPException.BadData
+
+@Command(
+    name = "dearmor",
+    resourceBundle = "msg_dearmor",
+    exitCodeOnInvalidInput = SOPGPException.UnsupportedOption.EXIT_CODE)
+class DearmorCmd : AbstractSopCmd() {
+
+    override fun run() {
+        val dearmor = throwIfUnsupportedSubcommand(SopCLI.getSop().dearmor(), "dearmor")
+
+        try {
+            dearmor.data(System.`in`).writeTo(System.out)
+        } catch (badData: BadData) {
+            val errorMsg = getMsg("sop.error.input.stdin_not_openpgp_data")
+            throw BadData(errorMsg, badData)
+        } catch (e: IOException) {
+            e.message?.let {
+                val errorMsg = getMsg("sop.error.input.stdin_not_openpgp_data")
+                if (it == "invalid armor" ||
+                    it == "invalid armor header" ||
+                    it == "inconsistent line endings in headers" ||
+                    it.startsWith("unable to decode base64 data")) {
+                    throw BadData(errorMsg, e)
+                }
+                throw RuntimeException(e)
+            }
+                ?: throw RuntimeException(e)
+        }
+    }
+}
