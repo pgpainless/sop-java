49120c5da883dca98ec243577b9368c9569dd858
Paul Schaub <vanitasvitae@fsfe.org>
2023-11-04 17:41:07 +0100

    Kotlin conversion: ChangeKeyPasswordCmd

---
+++ sop-java-picocli/src/main/kotlin/sop/cli/picocli/commands/ChangeKeyPasswordCmd.kt
@@ -0,0 +1,46 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package sop.cli.picocli.commands
+
+import java.io.IOException
+import java.lang.RuntimeException
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import sop.cli.picocli.SopCLI
+import sop.exception.SOPGPException
+
+@Command(
+    name = "change-key-password",
+    resourceBundle = "msg_change-key-password",
+    exitCodeOnInvalidInput = SOPGPException.UnsupportedOption.EXIT_CODE)
+class ChangeKeyPasswordCmd : AbstractSopCmd() {
+
+    @Option(names = ["--no-armor"], negatable = true) var armor: Boolean = true
+
+    @Option(names = ["--old-key-password"], paramLabel = "PASSWORD")
+    var oldKeyPasswords: List<String> = listOf()
+
+    @Option(names = ["--new-key-password"], arity = "0..1", paramLabel = "PASSWORD")
+    var newKeyPassword: String? = null
+
+    override fun run() {
+        val changeKeyPassword =
+            throwIfUnsupportedSubcommand(SopCLI.getSop().changeKeyPassword(), "change-key-password")
+
+        if (!armor) {
+            changeKeyPassword.noArmor()
+        }
+
+        oldKeyPasswords.forEach { changeKeyPassword.oldKeyPassphrase(it) }
+
+        newKeyPassword?.let { changeKeyPassword.newKeyPassphrase(it) }
+
+        try {
+            changeKeyPassword.keys(System.`in`).writeTo(System.out)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+}
