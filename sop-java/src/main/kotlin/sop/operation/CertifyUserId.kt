3104085fe715f359b77d1fb4f148117f57dc1ebc
Paul Schaub <vanitasvitae@fsfe.org>
2024-09-18 16:01:30 +0200

    Implement certify-userid command

---
+++ sop-java/src/main/kotlin/sop/operation/CertifyUserId.kt
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2024 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package sop.operation
+
+import sop.Ready
+import sop.exception.SOPGPException
+import sop.util.UTF8Util
+import java.io.IOException
+import java.io.InputStream
+
+interface CertifyUserId {
+
+    @Throws(SOPGPException.UnsupportedOption::class)
+    fun noArmor(): CertifyUserId
+
+    @Throws(SOPGPException.UnsupportedOption::class)
+    fun userId(userId: String): CertifyUserId
+
+    @Throws(SOPGPException.PasswordNotHumanReadable::class, SOPGPException.UnsupportedOption::class)
+    fun withKeyPassword(password: String): CertifyUserId = withKeyPassword(password.toByteArray(UTF8Util.UTF8))
+
+    @Throws(SOPGPException.PasswordNotHumanReadable::class, SOPGPException.UnsupportedOption::class)
+    fun withKeyPassword(password: ByteArray): CertifyUserId
+
+    @Throws(SOPGPException.UnsupportedOption::class)
+    fun noRequireSelfSig(): CertifyUserId
+
+    @Throws(SOPGPException.BadData::class, IOException::class, SOPGPException.KeyIsProtected::class)
+    fun keys(keys: InputStream): CertifyUserId
+
+    @Throws(SOPGPException.BadData::class, IOException::class, SOPGPException.KeyIsProtected::class)
+    fun keys(keys: ByteArray): CertifyUserId = keys(keys.inputStream())
+
+    @Throws(SOPGPException.BadData::class, IOException::class, SOPGPException.CertUserIdNoMatch::class)
+    fun certs(certs: InputStream): Ready
+
+    @Throws(SOPGPException.BadData::class, IOException::class, SOPGPException.CertUserIdNoMatch::class)
+    fun certs(certs: ByteArray): Ready = certs(certs.inputStream())
+}
