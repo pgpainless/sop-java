6d23d3771d310e538e7a49b1ba92a46279c619c9
Paul Schaub <vanitasvitae@fsfe.org>
2025-05-13 15:37:48 +0200

    Remove unused import

--- sop-java-testfixtures/src/main/java/sop/testsuite/operation/MergeCertsTest.java
+++ sop-java-testfixtures/src/main/java/sop/testsuite/operation/MergeCertsTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import sop.SOP;
-import sop.util.HexUtil;
 
 import java.io.IOException;
 import java.util.stream.Stream;
