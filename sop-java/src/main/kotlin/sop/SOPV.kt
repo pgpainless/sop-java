e680f3450a392aab2569fd97e6e993ba65fd63af
Paul Schaub <vanitasvitae@fsfe.org>
2025-07-25 12:21:06 +0200

    SOPV: Document since when operations are available

--- sop-java/src/main/kotlin/sop/SOPV.kt
+++ sop-java/src/main/kotlin/sop/SOPV.kt
@@ -12,27 +12,41 @@ import sop.operation.Version
 /** Subset of [SOP] implementing only OpenPGP signature verification. */
 interface SOPV {
 
-    /** Get information about the implementations name and version. */
+    /**
+     * Get information about the implementations name and version.
+     *
+     * @since sopv 1.0
+     */
     fun version(): Version?
 
     /**
      * Verify detached signatures. If you need to verify an inline-signed message, use
      * [inlineVerify] instead.
+     *
+     * @since sopv 1.0
      */
     fun verify(): DetachedVerify? = detachedVerify()
 
     /**
      * Verify detached signatures. If you need to verify an inline-signed message, use
      * [inlineVerify] instead.
+     *
+     * @since sopv 1.0
      */
     fun detachedVerify(): DetachedVerify?
 
     /**
      * Verify signatures of an inline-signed message. If you need to verify detached signatures over
      * a message, use [detachedVerify] instead.
+     *
+     * @since sopv 1.0
      */
     fun inlineVerify(): InlineVerify?
 
-    /** Validate a UserID in an OpenPGP certificate. */
+    /**
+     * Validate a UserID in an OpenPGP certificate.
+     *
+     * @since sopv 1.2
+     */
     fun validateUserId(): ValidateUserId?
 }
