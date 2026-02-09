ec41498d91a9f2b9bd791e591d49e0effb7be841
Paul Schaub <vanitasvitae@fsfe.org>
2026-02-09 16:12:34 +0100

    Assume passed in SOP implementation is ExternalSOP

--- external-sop/src/test/java/sop/testsuite/operation/ExternalVersionTest.java
+++ external-sop/src/test/java/sop/testsuite/operation/ExternalVersionTest.java
@@ -7,14 +7,17 @@ package sop.testsuite.operation;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import sop.SOP;
+import sop.external.ExternalSOP;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class ExternalVersionTest extends VersionTest {
 
     @ParameterizedTest
     @MethodSource("provideInstances")
     public void testExtendedVersionHasAppendedExternalSOPVersion(SOP sop) {
+        assumeTrue(sop instanceof ExternalSOP);
         String sopJavaVersion = sop.version().getSopJavaVersion();
 
         String extendedVersion = sop.version().getExtendedVersion();
