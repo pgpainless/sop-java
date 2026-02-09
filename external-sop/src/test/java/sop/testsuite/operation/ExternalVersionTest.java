ae7616b2b077480972f48111c9e7764ce9933fe7
Paul Schaub <vanitasvitae@fsfe.org>
2026-02-09 16:10:53 +0100

    Add test for external-sop version --extended

---
+++ external-sop/src/test/java/sop/testsuite/operation/ExternalVersionTest.java
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2026 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package sop.testsuite.operation;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import sop.SOP;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExternalVersionTest extends VersionTest {
+
+    @ParameterizedTest
+    @MethodSource("provideInstances")
+    public void testExtendedVersionHasAppendedExternalSOPVersion(SOP sop) {
+        String sopJavaVersion = sop.version().getSopJavaVersion();
+
+        String extendedVersion = sop.version().getExtendedVersion();
+        assertTrue(extendedVersion.endsWith("via external-sop " + sopJavaVersion));
+    }
+}
